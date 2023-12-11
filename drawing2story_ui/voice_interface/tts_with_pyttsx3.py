9e9ffb9d0aefa50ea20b4c9526da8737752f89c9
aisven <aisven822@gmail.com>
2023-12-11 01:00:07 +0100

    testing text-to-speech with pyttsx3

---
+++ drawing2story_ui/voice_interface/tts_with_pyttsx3.py
@@ -0,0 +1,49 @@
+import pyttsx3
+import logging
+
+
+def say_text_with_pyttsx3(text: str, driver_name: str, debug: bool):
+
+    # pyttsx3 is these days also known as py3-tts
+    # https://pypi.org/project/py3-tts/
+    # https: // py3 - tts.vigneshrao.com / support /
+
+    # pyttsx3 delegates to speech synthesis engines of the operating systems
+    # SAPI5 on Windows XP and Windows Vista and Windows 8,8.1 , 10
+    # NSSpeechSynthesizer on Mac OS X 10.5 (Leopard) and 10.6 (Snow Leopard)
+    # espeak on Ubuntu Desktop Edition 8.10 (Intrepid), 9.04 (Jaunty), and 9.10 (Karmic)
+
+    # it works offline i.e. it does not require any connection to some API
+
+    engine = pyttsx3.init(driverName=driver_name, debug=debug)
+    engine.say(text)
+    engine.runAndWait()
+
+
+def try_all_available_voices_with_pyttsx3(driver_name: str, debug: bool):
+    logger = logging.getLogger("ourlog")
+    logger.setLevel(logging.INFO)
+
+    engine = pyttsx3.init(driverName=driver_name, debug=debug)
+
+    # first let's reduce the rate
+    rate = engine.getProperty('rate')
+    logger.info(f"rate={rate}")
+    new_rate = rate * 0.8
+    logger.info(f"new_rate={new_rate}")
+    engine.setProperty('rate', new_rate)
+
+    # second let's reduce the volume
+    volume = engine.getProperty('volume')
+    logger.info(f"volume={volume}")
+    new_volume = volume * 0.9
+    logger.info(f"new_volume={new_volume}")
+    engine.setProperty('volume', new_volume)
+
+    voices = engine.getProperty('voices')
+    for voice in voices:
+        logger.info(f"-----")
+        logger.info(f"voice={voice}")
+        engine.setProperty('voice', voice.id)
+        engine.say('Hello. I am your computer. Have a nice day.')
+        engine.runAndWait()
---
+++ tests/voice_interface/test_tts_with_pyttsx3.py
@@ -0,0 +1,21 @@
+import pytest
+
+from drawing2story_ui.voice_interface.tts_with_pyttsx3 import say_text_with_pyttsx3
+from drawing2story_ui.voice_interface.tts_with_pyttsx3 import try_all_available_voices_with_pyttsx3
+
+# Windows
+#global_driver_name = "sapi5"
+# macOS
+global_driver_name = "nsss"
+# Ubuntu
+#global_driver_name = "espeak"
+
+
+def test_say_text_with_pyttsx3_on_macos():
+    text = "hello"
+    say_text_with_pyttsx3(text=text, driver_name=global_driver_name, debug=True)
+
+
+def test_try_all_available_voices_with_pyttsx3_on_macos():
+    text = "hello"
+    try_all_available_voices_with_pyttsx3(driver_name=global_driver_name, debug=True)
