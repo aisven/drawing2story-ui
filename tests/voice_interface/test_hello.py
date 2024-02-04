4736427345d47acac8b0474a1a622dffa47291c9
aisven <aisven822@gmail.com>
2024-02-05 00:41:14 +0100

    remove hello world program and test

--- tests/voice_interface/test_hello.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from drawing2story_ui.voice_interface.hello import hello
-
-
-def test_create_database_with_user():
-    hello_result = hello()
-    assert hello_result is not None
-    assert hello_result.startswith("Hello!")
