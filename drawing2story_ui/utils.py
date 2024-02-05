fc1760c0cd1b2334831038d1adf2e46fc7172d19
aisven <aisven822@gmail.com>
2024-02-05 01:03:57 +0100

    sentiment analysis with our heuristic

--- drawing2story_ui/utils.py
+++ drawing2story_ui/utils.py
@@ -1,8 +1,8 @@
+import base64
+import random
+
 from PIL import Image
 from io import BytesIO
-import base64
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import random
 
 
 def resize_image(image_data, max_size=(300, 300)):
@@ -16,36 +16,6 @@ def resize_image(image_data, max_size=(300, 300)):
     resized_image_data = base64.b64encode(img.tobytes()).decode("utf-8")
 
     return resized_image_data
-
-
-# def sentiment_analysis(text):
-#     # Create a TextBlob object
-#     blob = TextBlob(text)
-#     # Get the sentiment scores between -1 and 1
-#     sentiment = blob.sentiment.polarity
-#     # Get the sentiment label
-#     if sentiment > 0.5:
-#         label = "positive"
-#     elif sentiment < -0.5:
-#         label = "negative"
-#     else:
-#         label = "neutral"
-#     return sentiment, label
-
-
-def sentiment_analysis(text):
-    # Create a SentimentIntensityAnalyzer object
-    analyzer = SentimentIntensityAnalyzer()
-    # Get the sentiment scores between -1 and 1
-    sentiment = analyzer.polarity_scores(text)["compound"]
-    # Get the sentiment label
-    if sentiment > 0.0:
-        label = "positive"
-    elif sentiment < 0.0:
-        label = "negative"
-    else:
-        label = "neutral"
-    return sentiment, label
 
 
 def animal_detection(image_data):
